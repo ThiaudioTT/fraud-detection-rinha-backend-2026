944ff7d36d282ded5d211fd62bd98e3097cab5ec
Vaz <43919529+ThiaudioTT@users.noreply.github.com>
2026-05-30 10:03:28 -0300

    WIP: adding rules for embbeding

--- internal/normalizer/normalizer.go
+++ internal/normalizer/normalizer.go
@@ -13,13 +13,33 @@ func LimitValue(value, max float64) float32 {
 	return float32(value / max)
 }
 
-func NormalizePayloadTransaction(payload models.GetFraudScore) []float32 {
+// Return the embedded vector of the transaction
+func NormalizePayloadTransaction(payload models.FraudScoreRequest) []float32 {
 	vector := make([]float32, 14)
 
 	// amount
-	vector[0] = LimitValue(payload.Transaction.Amount, config.Cfg.MAX_AMOUNT)
+	vector[0] = LimitValue(payload.Transaction.Amount/config.Cfg.MAX_AMOUNT, config.Cfg.MAX_AMOUNT)
 	// installments
-	vector[1] = LimitValue(payload.Customer.AvgAmount, config.Cfg.MAX_AMOUNT)
+	vector[1] = LimitValue(float64(payload.Transaction.Installments)/float64(config.Cfg.MAX_INSTALLMENTS), float64(config.Cfg.MAX_INSTALLMENTS))
+	// amount vs avg
+	vector[2] = LimitValue((payload.Transaction.Amount/payload.Customer.AvgAmount)/config.Cfg.AMOUNT_VS_AVG_RATIO, config.Cfg.AMOUNT_VS_AVG_RATIO)
+	// hour of day
+	vector[3] = LimitValue(float64(payload.Transaction.RequestedAt.Hour())/23, 1)
+
+	// day_of_week
+	vector[4] = LimitValue(float64(payload.Transaction.RequestedAt.Weekday())/6, 1)
+
+	// when last transaction is not null
+	if payload.LastTransaction != nil {
+		// minutes since last transaction
+		vector[5] = LimitValue(float64(payload.Transaction.RequestedAt.Sub(payload.LastTransaction.Timestamp).Minutes())/float64(config.Cfg.MAX_MINUTES), 1)
+		// km from last transaction
+		vector[6] = LimitValue(payload.LastTransaction.KmFromCurrent/config.Cfg.MAX_KM, 1)
+	} else {
+		// If no last transaction, we put -1
+		vector[5] = -1
+		vector[6] = -1
+	}
 
 	return vector
 }
