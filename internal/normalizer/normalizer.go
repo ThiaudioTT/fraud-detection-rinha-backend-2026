2bf8e44a91012fd39668e6b926cbf3b519bd7653
Vaz <43919529+ThiaudioTT@users.noreply.github.com>
2026-05-30 00:44:35 -0300

    feat: implement value limiting function and update transaction normalization logic

--- internal/normalizer/normalizer.go
+++ internal/normalizer/normalizer.go
@@ -1,8 +1,25 @@
 package normalizer
 
-import "fraud-detection-2026/internal/models"
+import (
+	"fraud-detection-2026/internal/config"
+	"fraud-detection-2026/internal/models"
+)
+
+// If number > max, return 1, else return number / max
+func LimitValue(value, max float64) float32 {
+	if value > max {
+		return 1
+	}
+	return float32(value / max)
+}
 
 func NormalizePayloadTransaction(payload models.GetFraudScore) []float32 {
+	vector := make([]float32, 14)
 
-	return []float32{}
+	// amount
+	vector[0] = LimitValue(payload.Transaction.Amount, config.Cfg.MAX_AMOUNT)
+	// installments
+	vector[1] = LimitValue(payload.Customer.AvgAmount, config.Cfg.MAX_AMOUNT)
+
+	return vector
 }
