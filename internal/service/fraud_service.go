54f7ca33cd8d56106e8b31d1df43b6a632e07977
Vaz <43919529+ThiaudioTT@users.noreply.github.com>
2026-05-30 12:02:21 -0300

    fix: correct struct and method naming to follow Go conventions in fraudService

--- internal/service/fraud_service.go
+++ internal/service/fraud_service.go
@@ -2,12 +2,12 @@ package service
 
 import "github.com/jackc/pgx/v5/pgxpool"
 
-// FraudService provides methods to calculate fraud scores.
+// fraudService provides methods to calculate fraud scores.
 // Mainly returns if a embbed transactions is fraud or not, and the fraud score itself.
-type FraudService struct {
+type fraudService struct {
 	db *pgxpool.Pool
 }
 
-func NewFraudService(db *pgxpool.Pool) *FraudService {
-	return &FraudService{db: db}
+func NewFraudService(db *pgxpool.Pool) *fraudService {
+	return &fraudService{db: db}
 }
