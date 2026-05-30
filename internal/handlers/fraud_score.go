612a9c7d8a45750afbbb00dd074eb6d746c1ad22
Vaz <43919529+ThiaudioTT@users.noreply.github.com>
2026-05-30 12:22:38 -0300

    feat: implement scoring logic in FraudScore handler and update fraudService methods

--- internal/handlers/fraud_score.go
+++ internal/handlers/fraud_score.go
@@ -5,6 +5,8 @@ import (
 
 	"fraud-detection-2026/internal/models"
 	"fraud-detection-2026/internal/normalizer"
+	"fraud-detection-2026/internal/service"
+	"fraud-detection-2026/pkg/database"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,10 +19,15 @@ func FraudScore(c *gin.Context) {
 	}
 
 	normalizedPayload := normalizer.NormalizePayloadTransaction(payload)
+	fraudService := service.NewFraudService(database.Pool) // FIXME: this should be injected, not created here
+	approved, fraudScore, err := fraudService.ScoreTransaction(c.Request.Context(), normalizedPayload)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "failed to score fraud", "error": err.Error()})
+		return
+	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"approved":    false,
-		"fraud_score": 1.0,
-		"payload":     normalizedPayload, // TODO: remove this line after testing
+		"approved":    approved,
+		"fraud_score": fraudScore,
 	})
 }
--- internal/service/fraud_service.go
+++ internal/service/fraud_service.go
@@ -1,6 +1,12 @@
 package service
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+
+	"fraud-detection-2026/internal/repo"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 // fraudService provides methods to calculate fraud scores.
 // Mainly returns if a embbed transactions is fraud or not, and the fraud score itself.
@@ -10,4 +16,24 @@ type fraudService struct {
 
 func NewFraudService(db *pgxpool.Pool) *fraudService {
 	return &fraudService{db: db}
+}
+
+func (s *fraudService) ScoreTransaction(ctx context.Context, embedding []float32) (bool, float32, error) {
+	referenceRepo := repo.NewReferenceRepo(s.db) // FIXME: this should be injected, not created here
+	matches, err := referenceRepo.FindSimilarTransactions(ctx, embedding)
+	if err != nil {
+		return false, 0, err
+	}
+
+	var fraudCount float32
+	for _, match := range matches {
+		if match.IsFraud {
+			fraudCount++
+		}
+	}
+
+	fraudScore := fraudCount / float32(len(matches))
+	approved := fraudScore < 0.6
+
+	return approved, fraudScore, nil // FIXME: I should be a struct
 }
